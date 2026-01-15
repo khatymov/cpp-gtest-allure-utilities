110a5f8f0a883f20c1e4773a7e4f1b18a5428bdd
Rene <khatymovrenat@gmail.com>
2026-01-15 19:38:31 +0300

    Fix

--- src/GTestAllureUtilities/cmake/GTestAllureUtilitiesConfig.cmake
+++ src/GTestAllureUtilities/cmake/GTestAllureUtilitiesConfig.cmake
@@ -2,6 +2,9 @@
 
 # If you ever add external dependencies that should be auto-found for users,
 # you can add find_dependency() calls here.
+include(CMakeFindDependencyMacro)
+find_dependency(GTest)
+find_dependency(RapidJSON)
 
 include("${CMAKE_CURRENT_LIST_DIR}/GTestAllureUtilitiesTargets.cmake")
 
